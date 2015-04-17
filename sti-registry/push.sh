4679646c49ed0c6a39a40c9f5090143b72080d05
root <root@uil0map-paas-app02.skead.no>
2015-04-17 14:52:19 +0200

    docker downloads zip

--- sti-registry/push.sh
+++ sti-registry/push.sh
@@ -1,3 +1,3 @@
-docker tag skatt/registry 172.30.17.172:5000/skatt/registry
-docker push 172.30.17.172:5000/skatt/registry
+docker tag -f skatt/registry 172.30.17.172:5000/skatt/registry
+docker push -f 172.30.17.172:5000/skatt/registry
 
